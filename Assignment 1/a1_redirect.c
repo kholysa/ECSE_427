360f313464d63cb9f6be1e180135aa4e2c7af6fd
Saif Elkholy <saif@districtm.ca>
2018-02-21 13:00:08 -0500

    Finished ass1-93

--- Assignment 1/a1_redirect.c
+++ Assignment 1/a1_redirect.c
@@ -1,38 +1,33 @@
-/*
------------------ COMP 310/ECSE 427 Winter 2018 -----------------
-I declare that the awesomeness below is a genuine piece of work
-and falls under the McGill code of conduct, to the best of my knowledge.
------------------------------------------------------------------
-*/ 
-
-//Please enter your name and McGill ID below
-//Name: <Saif Elkholy>
-//McGill ID: <260606967>
-
-#include <stdio.h>
-#include <unistd.h>
+#include<stdio.h> 
+#include<unistd.h>
 #include <fcntl.h>
 #include <string.h>
 
-int main ()
-{
-    //Create new text file
-    FILE* file_ptr = fopen("redirect_out.txt", "w");
-    //getting fileDescriptor value for newly created txt file
-    int fileDescriptor = open("redirect_out.txt", O_WRONLY);
-    //making a copy of the stdout file descriptor
-    int stdOutCopy = dup(1);
+int main() {
 
-    printf("First : Print to stdout \n");
+FILE* file_ptr = fopen("redirect_out.txt", "w");
 
-    int pipedFileDescriptors = dup2(fileDescriptor, 1);
-    if (pipedFileDescriptors < 0)
-        return 0;
-    printf("Second : Print to redirect_out.txt \n");
+printf("First : Print to stdout\n");
+
+int fileHandleNumber = open("redirect_out.txt", O_WRONLY);
+
+int defOutCopy = dup(1);
+
+close (1);
+
+dup2(fileHandleNumber, 1);
+
+printf ("Second : Print to redirect_out.txt\n");
+
+close(1);
+
+close (fileHandleNumber);
+
+dup2(defOutCopy, 1);
     
-    dup2(stdOutCopy, 1);
-    printf ("Third : Print to stdout \n");
-    
-    close(fileDescriptor);
-    return 0;
+close(defOutCopy);
+
+printf ("Third : Print to stdout\n");
+
+return 0; 
 }
