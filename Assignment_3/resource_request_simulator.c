9fb840fe4c2612834dbc86ae58d579f817dd9cf8
Saif Elkholy <saif@districtm.ca>
2018-04-03 21:53:59 -0400

    Reached the ppart where i can start worrying about bankers smh rip yolo

---
+++ Assignment_3/resource_request_simulator.c
@@ -0,0 +1,114 @@
+/*
+getUserInput() //get: 
+- number of processes
+- number of resourecs types
+- number of each resource type
+- maximum resource claim per process
+
+Each process will be a thread:
+- process i start
+- while (true) request[i,j]:
+    - i is a random number from 0->remaining amount of  resources for this process
+    - j is the resource type
+- boolean isSafe = BANKERS(&request, global resources in system)
+- if isSafe{aquire resources and check for process termination}
+- else process is blocked, continue the execution
+*/
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <semaphore.h>
+#include <stdbool.h>
+
+pthread_mutex_t MUTEX = PTHREAD_MUTEX_INITIALIZER;
+
+int PROCESSESS;
+int RESOURCE_TYPES;
+int *RESOURCE_TYPE_AVAILABILITIES;
+int *PROCESS_REQUESTS;
+
+int *HOLD;
+int *NEED;
+int *REQ;
+
+void getUserInput()
+{
+    printf("How many processess would you like to create?\n");
+    scanf("%d", &PROCESSESS);
+
+    printf("How many resource types would you like to create?\n");
+    scanf("%d", &RESOURCE_TYPES);
+
+    RESOURCE_TYPE_AVAILABILITIES = (int *)malloc(sizeof (int) * RESOURCE_TYPES);
+    for (int i=0; i < RESOURCE_TYPES; i++){
+        printf("How many resources for resource type: %d would you like to create?\n", i);
+        scanf("%d", &*RESOURCE_TYPE_AVAILABILITIES + i);
+    }
+
+    // int PROCESS_REQUESTS[PROCESSESS][RESOURCE_TYPES];
+    PROCESS_REQUESTS =(int *)malloc(sizeof (int) * PROCESSESS * RESOURCE_TYPES);
+    for(int i=0; i< PROCESSESS; i++){
+        for(int j=0; j< RESOURCE_TYPES; j++){
+            printf("Specfiy how many resources of type %d, you would like process %d to use:\n",j, i);
+            scanf("%d", &*(PROCESS_REQUESTS + i*RESOURCE_TYPES + j));
+        }
+    }
+}
+
+void *fnProcess(void* pr_id)
+{
+    // Each process will be a thread:
+    // - process i start
+    // - while (true) request[i,j]:
+    //     - i is a random number from 0->remaining amount of  resources for this process
+    //     - j is the resource type
+    // - boolean isSafe = BANKERS(&request, global resources in system)
+    // - if isSafe{aquire resources and check for process termination}
+    // - else process is blocked, continue the execution
+    int *processId = (int *) pr_id;
+    // while (true){
+        int requestVector[RESOURCE_TYPES];
+        for(int i=0; i < RESOURCE_TYPES; i++){
+            int remainingAmountOfResourcesForThisProcessAndResourceType = *(NEED + (*processId)*RESOURCE_TYPES + i);
+            printf("Process %d needs %d more resources of type %d", *processId, remainingAmountOfResourcesForThisProcessAndResourceType, i);
+            int i = rand() % remainingAmountOfResourcesForThisProcessAndResourceType;
+        }
+    // }
+}
+
+int main()
+{
+    getUserInput();
+
+    HOLD =(int *)malloc(sizeof (int) * PROCESSESS * RESOURCE_TYPES);
+    NEED =(int *)malloc(sizeof (int) * PROCESSESS * RESOURCE_TYPES);
+    for(int i=0; i<PROCESSESS; i++){
+        for(int j=0; j< RESOURCE_TYPES; j++){
+            *(HOLD + i*RESOURCE_TYPES + j) = 0;
+            *(NEED + i*RESOURCE_TYPES + j) = *(PROCESS_REQUESTS + i*RESOURCE_TYPES + j);
+        }
+    }
+
+    pthread_mutex_init(&MUTEX, NULL);
+    pthread_t * processThreads = malloc(sizeof(pthread_t)*PROCESSESS);
+    int *process_ids[PROCESSESS];
+
+    for(int j =0; j < PROCESSESS; j++) {
+        process_ids[j] = malloc(sizeof(int));
+        *process_ids[j] = j;
+        // Print an error if one occurs
+        printf("Creating process thread %d\n", j);
+        if (pthread_create(&processThreads[j], NULL, fnProcess, process_ids[j]) != 0 ){
+            printf("Error with pthread create for process %i\n", j);
+        }
+    }
+    for(int j =0; j < PROCESSESS; j++) {
+        pthread_join(processThreads[j], NULL);
+    }
+    pthread_exit(NULL);
+
+    return 0;
+}
